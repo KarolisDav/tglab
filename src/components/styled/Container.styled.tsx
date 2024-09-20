1ff52b4d1d8aab8f3e62afb5fcfcf85efa3d94d0
Karolis <davlijevas.karolis@gmail.com>
2024-09-20 12:08:18 +0300

    add responsiveness for mid screens

--- src/components/styled/Container.styled.tsx
+++ src/components/styled/Container.styled.tsx
@@ -1,7 +1,16 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 1440px;
+  width: 1345px;
   max-width: 100%;
   margin: 0 auto;
+  @media (max-width: 1345px) {
+    width: 800px;
+  }
+  @media (max-width: 1075px) {
+    width: 800px;
+  }
+  @media (max-width: 805px) {
+    width: 530px;
+  }
 `;
