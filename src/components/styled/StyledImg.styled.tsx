b336dc8cc74c41fab621ed3fa85d9e6d13f28b30
Karolis <davlijevas.karolis@gmail.com>
2024-09-20 11:54:45 +0300

    Add responsiveness for mobile screens

--- src/components/styled/StyledImg.styled.tsx
+++ src/components/styled/StyledImg.styled.tsx
@@ -4,4 +4,8 @@ export const StyledImg = styled.img`
   width: 250px;
   height: 250px;
   object-fit: cover;
+  @media (max-width: 535px) {
+    width: 100%;
+    height: auto;
+  }
 `;
--- src/components/styled/StyledLi.styled.tsx
+++ src/components/styled/StyledLi.styled.tsx
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
-export const StyledLi = styled.div`
+export const StyledLi = styled.li`
   width: 250px;
   margin-bottom: 20px;
+  @media (max-width: 535px) {
+    width: 100%;
+  }
 `;
